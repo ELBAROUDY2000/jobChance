0ab04ecdbf59d45a753a72a782388aea7fedd961
Ahmed Shaaban Abdelaziz Yousef <94333814+ELBAROUDY2000@users.noreply.github.com>
2022-01-01 17:51:33 +0200

    Delete src directory

--- src/component/styles/UpdateCompanyDataStyled.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-export const UpdateCompanyDataStyled=styled.form`
-background-color: rgb(216, 216, 216);
-background-repeat: no-repeat;
-background-size: cover;
-height:100vh;
-font-family: 'Times New Roman', Times, serif;
-`
